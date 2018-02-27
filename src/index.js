0535c4cf77da941d5c1fdd19260760f98e2195a0
Hebert Porto <hebertporto@gmail.com>
2018-02-26 21:55:48 -0300

    voltando redux persist

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { persistStore } from 'redux-persist'
 
 import history from './config/historyRouter'
 
-// import configureStore from './config/store'
 import store from './config/store'
 import App from './modules/App'
 
@@ -23,23 +22,3 @@ persistStore(store, {
     </Provider>
   ), document.getElementById('root'))
 })
-
-// async function init () {
-//   const store = await configureStore()
-//   return persistStore(store, {
-//     whitelist: [
-//       'team',
-//       'user'
-//     ]
-//   }, () => {
-//     render((
-//       <Provider store={store}>
-//         <Router history={history}>
-//           <App />
-//         </Router>
-//       </Provider>
-//     ), document.getElementById('root'))
-//   }).purge()
-// }
-
-// init()
