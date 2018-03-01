0f1653807e7098935100634af09326a0a891c31a
Hebert Porto <hebertporto@gmail.com>
2018-02-28 21:46:11 -0300

    component client column

---
+++ src/modules/dash/components/ColumnClient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ColumnClient = props => {
+  return (
+    <div className='pd-column-1'>
+      <h1>Column 1 - Client</h1>
+    </div>
+  )
+}
--- src/modules/dash/components/ProductivityColumns.jsx
+++ src/modules/dash/components/ProductivityColumns.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Card, Image } from 'semantic-ui-react'
 
 import '../style/ProductivityColumns.css'
+import { ColumnClient } from './ColumnClient'
 
 class ProductivityColumns extends Component {
   state = {
@@ -11,44 +11,7 @@ class ProductivityColumns extends Component {
   render () {
     return (
       <div className="pd-wrapper">
-        <div className='pd-column-1'>
-          <Card>
-            <Card.Content>
-              <Image
-                floated='right'
-                size='mini'
-                src='https://www.mystellar.org/uploads/avatars/avatar_3508.jpg?dateline=1495441932'
-              />
-              <Card.Header>
-                Steve Sanders
-              </Card.Header>
-              <Card.Meta>
-                Friends of Elliot
-              </Card.Meta>
-              <Card.Description>
-                Steve wants to add you to the group <strong>best friends</strong>
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Image
-                floated='right'
-                size='mini'
-                src='https://www.mystellar.org/uploads/avatars/avatar_3508.jpg?dateline=1495441932'
-              />
-              <Card.Header>
-                Steve Sanders
-              </Card.Header>
-              <Card.Meta>
-                Friends of Elliot
-              </Card.Meta>
-              <Card.Description>
-                Steve wants to add you to the group <strong>best friends</strong>
-              </Card.Description>
-            </Card.Content>
-          </Card>
-        </div>
+        <ColumnClient />
         <div className='pd-column-2'>
           <h1>Column 2</h1>
         </div>
